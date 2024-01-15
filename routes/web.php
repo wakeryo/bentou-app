0e74353cb9daaae40fb5db07b75ff42d9fd5ff3b
wakeryo <ryo0301kw@yahoo.co.jp>
2024-01-16 01:58:32 +0900

    ルートの設定がうまくいかない

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CalenderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('',CalenderController::class,'')
+
+Route::get('/x'function () {
+    return view('index');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
