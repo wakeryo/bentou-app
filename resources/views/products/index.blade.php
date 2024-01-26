695549f7ee0fad69b537b6c7391ee1476b028d5d
wakeryo <ryo0301kw@yahoo.co.jp>
2024-01-26 21:14:47 +0900

    index.blade.phpの表示がうまくできない

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
- <html lang="ja">
- 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>お弁当.com</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+     <style>
+      table {
+        border-collapse: collapse;
+      }
+      td {
+        border: solid 1px black;
+        padding: 3px;
+      }
+    </style>
  </head>
- 
  <body>
-     <th>{{ $week }}{{ $now }}<th>
+  <div>aibjfab</div>
+  <div>
+  <table>
+    <!-- 各段を繰り返し出力 -->
+    <?php for($i = 1; $i <= 9; $i++){ ?>
+      <tr>
+        <!-- 各段の一つ一つのマスを繰り返し出力 -->
+        <?php for($j = 1; $j <= 9; $j++){ ?>
+          <td><?php echo $i; ?> * <?php echo $j; ?> = <?php echo $i * $j; ?></td>
+        <?php } ?>
+      </tr>
+    <?php } ?>
+  </table>
+
+  </div>
  </body>
  
  </html>
--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/products', [CalenderController::class, 'index']);
+
 require __DIR__.'/auth.php';
